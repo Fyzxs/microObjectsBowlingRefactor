149cc17caa76a8871bdf9cc674dbf822a716438d
Fyzxs <Fyzxs@users.noreply.github.com>
2017-10-13 15:29:24 -0700

    PinsDown Created

--- Game.cs
+++ Game.cs
@@ -1,11 +1,28 @@
 ﻿namespace BowlingKataMicroObjectsRefactor
 {
-    public class Game
+    public class PinsDown : IPinsDown
     {
         private readonly int[] _rolls = new int[21];
         private int _ptr;
-
         public void Roll(int pins) => _rolls[_ptr++] = pins;
+    }
+
+    public interface IPinsDown
+    {
+        void Roll(int pins);
+    }
+
+    public class Game
+    {
+        private readonly IPinsDown _pinsDown = new PinsDown();
+        private readonly int[] _rolls = new int[21];
+        private int _ptr;
+
+        public void Roll(int pins)
+        {
+            _pinsDown.Roll(pins);
+            _rolls[_ptr++] = pins;
+        }
 
         public int Score()
         {
