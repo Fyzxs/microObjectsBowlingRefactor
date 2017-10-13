e2289f9c4b2e497b904038fb06e0afc893f21db7
Fyzxs <Fyzxs@users.noreply.github.com>
2017-10-13 16:31:10 -0700

    Moved FrameState to own file

---
+++ FrameState.cs
@@ -0,0 +1,28 @@
+﻿namespace BowlingKataMicroObjectsRefactor
+{
+    public interface IFrameState
+    {
+        IFrameState Score(IPinsDown pinsDown, IFrameScore frameScore, IIndexAdjustment indexAdjustment);
+        int Score();
+    }
+
+    public class FrameState : IFrameState
+    {
+        private readonly int _score;
+        private readonly int _pinsIndex;
+
+        public FrameState(int score, int pinsIndex)
+        {
+            _score = score;
+            _pinsIndex = pinsIndex;
+        }
+
+        public IFrameState Score(IPinsDown pinsDown, IFrameScore frameScore, IIndexAdjustment indexAdjustment)
+            => new FrameState(Score(pinsDown, frameScore), PinsIndex(indexAdjustment));
+
+        public int Score() => _score;
+
+        private int PinsIndex(IIndexAdjustment indexAdjustment) => _pinsIndex + indexAdjustment.Adjustment();
+        private int Score(IPinsDown pinsDown, IFrameScore frameScore) => _score + frameScore.Score(pinsDown, _pinsIndex);
+    }
+}
