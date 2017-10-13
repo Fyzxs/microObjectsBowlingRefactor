3c8ab341533e869786f2f9129a7acba68c35be85
Fyzxs <Fyzxs@users.noreply.github.com>
2017-10-13 15:33:08 -0700

    Game#_rolls removed

--- Game.cs
+++ Game.cs
@@ -5,24 +5,20 @@
         private readonly int[] _rolls = new int[21];
         private int _ptr;
         public void Roll(int pins) => _rolls[_ptr++] = pins;
+        public int PinsDownAt(int pinsIndex) => _rolls[pinsIndex];
     }
 
     public interface IPinsDown
     {
         void Roll(int pins);
+        int PinsDownAt(int pinsIndex);
     }
 
     public class Game
     {
         private readonly IPinsDown _pinsDown = new PinsDown();
-        private readonly int[] _rolls = new int[21];
-        private int _ptr;
 
-        public void Roll(int pins)
-        {
-            _pinsDown.Roll(pins);
-            _rolls[_ptr++] = pins;
-        }
+        public void Roll(int pins) => _pinsDown.Roll(pins);
 
         public int Score()
         {
@@ -49,10 +45,10 @@
             return score;
         }
 
-        private bool IsStrike(int pinsIndex) => _rolls[pinsIndex] == 10;
-        private bool IsSpare(int pinsIndex) => _rolls[pinsIndex] + _rolls[pinsIndex + 1] == 10;
-        private int RegularScore(int pinsIndex) => _rolls[pinsIndex] + _rolls[pinsIndex + 1];
-        private int SpareScore(int pinsIndex) => 10 + _rolls[pinsIndex + 2];
-        private int StrikeScore(int pinsIndex) => 10 + _rolls[pinsIndex + 1] + _rolls[pinsIndex + 2];
+        private bool IsStrike(int pinsIndex) => _pinsDown.PinsDownAt(pinsIndex) == 10;
+        private bool IsSpare(int pinsIndex) => _pinsDown.PinsDownAt(pinsIndex) + _pinsDown.PinsDownAt(pinsIndex + 1) == 10;
+        private int RegularScore(int pinsIndex) => _pinsDown.PinsDownAt(pinsIndex) + _pinsDown.PinsDownAt(pinsIndex + 1);
+        private int SpareScore(int pinsIndex) => 10 + _pinsDown.PinsDownAt(pinsIndex + 2);
+        private int StrikeScore(int pinsIndex) => 10 + _pinsDown.PinsDownAt(pinsIndex + 1) + _pinsDown.PinsDownAt(pinsIndex + 2);
     }
 }
