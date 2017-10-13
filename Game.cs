d5a3f7c3740ccc6cad5c2eecea7bbefd3f80d774
Fyzxs <Fyzxs@users.noreply.github.com>
2017-10-13 15:52:58 -0700

    Moved IndexAdjustments to own file

--- Game.cs
+++ Game.cs
@@ -1,23 +1,5 @@
 ﻿namespace BowlingKataMicroObjectsRefactor
 {
-    public class StrikeIndexAdjustment : IIndexAdjustment
-    {
-        public int Adjustment() => 1;
-    }
-    public class SpareIndexAdjustment : IIndexAdjustment
-    {
-        public int Adjustment() => 2;
-    }
-    public class DefaultIndexAdjustment : IIndexAdjustment
-    {
-        public int Adjustment() => 2;
-    }
-
-    public interface IIndexAdjustment
-    {
-        int Adjustment();
-    }
-
     public class Game
     {
         private readonly IPinsDown _pinsDown;
---
+++ IndexAdjustment.cs
@@ -0,0 +1,21 @@
+﻿namespace BowlingKataMicroObjectsRefactor
+{
+    public interface IIndexAdjustment
+    {
+        int Adjustment();
+    }
+
+    public class StrikeIndexAdjustment : IIndexAdjustment
+    {
+        public int Adjustment() => 1;
+    }
+    public class SpareIndexAdjustment : IIndexAdjustment
+    {
+        public int Adjustment() => 2;
+    }
+    public class DefaultIndexAdjustment : IIndexAdjustment
+    {
+        public int Adjustment() => 2;
+    }
+
+}
