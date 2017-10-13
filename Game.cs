ecc1bbac7e60a8cf62d9443feaf522dc2102a3fd
Fyzxs <Fyzxs@users.noreply.github.com>
2017-10-13 16:10:29 -0700

    Added ScoreType file

--- Game.cs
+++ Game.cs
@@ -1,22 +1,5 @@
 ﻿namespace BowlingKataMicroObjectsRefactor
 {
-    public class IsStrike : IScoreType
-    {
-        public bool IsType(IPinsDown pinsDown, int pinsIndex) => pinsDown.PinsDownAt(pinsIndex) == 10;
-    }
-    public class IsSpare : IScoreType
-    {
-        public bool IsType(IPinsDown pinsDown, int pinsIndex) => pinsDown.PinsDownAt(pinsIndex) + pinsDown.PinsDownAt(pinsIndex + 1) == 10;
-    }
-    public class IsDefault : IScoreType
-    {
-        public bool IsType(IPinsDown pinsDown, int pinsIndex) => pinsDown.PinsDownAt(pinsIndex) + pinsDown.PinsDownAt(pinsIndex + 1) < 10;
-    }
-
-    public interface IScoreType
-    {
-        bool IsType(IPinsDown pinsDown, int pinsIndex);
-    }
 
     public class Game
     {
---
+++ ScoreType.cs
@@ -0,0 +1,19 @@
+﻿namespace BowlingKataMicroObjectsRefactor
+{
+    public interface IScoreType
+    {
+        bool IsType(IPinsDown pinsDown, int pinsIndex);
+    }
+    public class IsStrike : IScoreType
+    {
+        public bool IsType(IPinsDown pinsDown, int pinsIndex) => pinsDown.PinsDownAt(pinsIndex) == 10;
+    }
+    public class IsSpare : IScoreType
+    {
+        public bool IsType(IPinsDown pinsDown, int pinsIndex) => pinsDown.PinsDownAt(pinsIndex) + pinsDown.PinsDownAt(pinsIndex + 1) == 10;
+    }
+    public class IsDefault : IScoreType
+    {
+        public bool IsType(IPinsDown pinsDown, int pinsIndex) => pinsDown.PinsDownAt(pinsIndex) + pinsDown.PinsDownAt(pinsIndex + 1) < 10;
+    }
+}
